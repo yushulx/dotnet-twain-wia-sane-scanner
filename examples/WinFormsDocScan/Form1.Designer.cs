b9d5b35d9f9c7fa5d7c7d2ec77512cd4158c11af
yushulx <lingxiao1002@gmail.com>
2023-10-10 11:29:09 +0800

    Added WinForm example

---
+++ examples/WinFormsDocScan/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WinFormsDocScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(183, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Get Devices";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownHeight = 70;
+            comboBox1.DropDownWidth = 140;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.IntegralHeight = false;
+            comboBox1.Location = new Point(300, 30);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(131, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(440, 30);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Scan";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(63, 74);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(635, 558);
+            flowLayoutPanel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(762, 665);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private ComboBox comboBox1;
+        private Button button2;
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
